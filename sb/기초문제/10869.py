1394bb0da058e4b6e269bcc56c8f40402f2ce8f0
chobae <tmsprqo@gmail.com>
2022-09-23 19:09:35 +0900

    위치 변경

--- sb/기초문제/10869.py
+++ sb/기초문제/10869.py
@@ -2,4 +2,4 @@
 oper = ['+', '-', '*', '/', '%']
 a,b = map(str, input().split())
 for i in range(5):
-    print(int(eval(a+oper[i]+b)))
+    print(int(eval(a+oper[i]+b))) # eval함수 -> 'a+b'같은 문자열도 연산해줌!
